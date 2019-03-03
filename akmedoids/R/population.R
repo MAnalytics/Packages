f1eb1062400f07afb76bf232bc24e55c0250a945
geoMADE <geomad@leeds.ac.uk>
2019-03-03 18:06:58 +0000

    updates

---
+++ akmedoids/R/population.R
@@ -0,0 +1,9 @@
+#' @title sample population (denominator) data
+#' @description Simulated denominator data with missing observation
+#' @name population
+#' @docType data
+#' @usage population
+#' @format A matrix
+#' @keywords datasets
+
+NULL
